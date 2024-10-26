6508f267988b5c9002ed9d3cb8758b49e08f5c22
Gustavo Linares Villegas <guslinaresv@gmail.com>
2024-10-26 16:44:46 -0600

    Add: endpoint post employee

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 import 'dotenv/config';
 
-export const pool = await mysql.createConnection({
+export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
--- server/src/middlewares/employee.middlewares.js
+++ server/src/middlewares/employee.middlewares.js
@@ -1,3 +1,16 @@
-export const createEmployee = (req, res) => {
+import { pool } from "../db/db.js";
+
+export const createEmployee = async (req, res) => {
+  const { name, ageOld, country, position, age} = req.body;
+  const query = `INSERT INTO employee (name, ageOld, country, position, age) VALUES (?, ?, ?, ?, ?, NOW())`;
+  try {
+    const [result] = await pool.query(query, [name, ageOld, country, position, age]);
+
+    res.status(201).json({ message: 'Empleado creado', id: result.insertId });
+
+  } catch (error) {
+    console.error('Error al crear empleado: ', error);
+    res.status(500).json({ message: 'Error al crear empleado' });
+  }
   res.json({ message: 'Acceso a la ruta de creación de empleado' });
 };
